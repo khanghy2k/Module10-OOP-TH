787979fb3bef5f6ed12509a17f40e105fbb2773f
khanghy2k@gmail.com <khanghy2k@gmail.com>
2023-04-12 23:01:11 +0700

    update

---
+++ Date.js
@@ -0,0 +1,27 @@
+class Date {
+    constructor(day,month,year) {
+        this.day = day;
+        this.month = month;
+        this.year = year ;
+
+    }
+    getDay() {
+        return this.day
+    }
+    getMonth(){
+        return this.month
+    }
+    getYear() {
+        return this.year
+    }
+    setDay(day){
+        return this.day = day;
+    }
+    setMonth(month){
+        return this.month = month;
+    }
+    setYear(year){
+        return this.year = year;
+    }
+
+}
